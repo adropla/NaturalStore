712b3755467ccff8eddb1d4fdffb5e62e6b132d8
AnthonyWorldGuide <59710937+AnthonyWorldGuide@users.noreply.github.com>
2021-01-19 16:26:35 +0300

    Admin

--- Service/AdminAreaAuthorization.cs
+++ Service/AdminAreaAuthorization.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
-namespace MyCompany.Service
+namespace Natural_Store.Service
 {
     public class AdminAreaAuthorization : IControllerModelConvention
     {
