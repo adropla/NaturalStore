1c34bdde9c4a1e229c414e1a758eb1440674ccdc
adropla <sao.bur@yandex.ru>
2021-01-19 16:28:45 +0300

    migrations add

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,21 +8,9 @@ namespace NaturalStore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly DataManager dataManager;
-        
-        public HomeController(DataManager dataManager)
-        {
-            this.dataManager = dataManager;
-        }
-        
         public IActionResult Index()
         {
-            return View(dataManager.TextFields.GetTextFieldsByCodeWord("PageIndex"));
-        }
-        
-        public IActionResult Contacts()
-        {
-            return View(dataManager.TextFields.GetTextFieldsByCodeWord("PageContacts"));
+            return View();
         }
     }
 }
