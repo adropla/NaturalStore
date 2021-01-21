3109e9684bc1dc7db2ef4322fca685c381f09051
adropla <sao.bur@yandex.ru>
2021-01-21 09:03:37 +0300

    last

--- Migrations/20210121055453_onemore.Designer.cs
+++ Migrations/20210121055453_onemore.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NaturalStore.Domain;
 
 namespace NaturalStore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210121055453_onemore")]
+    partial class onemore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210121055453_onemore.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NaturalStore.Migrations
+{
+    public partial class onemore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ServiceID",
+                table: "OrderDetail",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "OrderDetail",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Order",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Order",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Order",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Order",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                column: "ConcurrencyStamp",
+                value: "016b1d81-3ba2-47d0-a367-535d968d8df2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "ae9a7e39-88e4-485d-8ca5-6aa6169b94f0", "AQAAAAEAACcQAAAAEDkfeIqFLrfIdOAqVnY6XHGfwNfD0eNOA5/MQdIKOBJORg3xal+Jm/BgWT0XXZRg6A==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("4aa76a4c-c59d-409a-84c1-06e6487a137a"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 21, 5, 54, 52, 838, DateTimeKind.Utc).AddTicks(3232));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 21, 5, 54, 52, 837, DateTimeKind.Utc).AddTicks(7073));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 21, 5, 54, 52, 838, DateTimeKind.Utc).AddTicks(3041));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ServiceID",
+                table: "OrderDetail",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "OrderDetail",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                column: "ConcurrencyStamp",
+                value: "0acef1a0-a6c1-4139-9882-7c1e87afb7ca");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "6411392a-24fd-4a9e-af86-ce5492d2bf3e", "AQAAAAEAACcQAAAAELRyUPaPbktmEwGcML1u9HTtDfkvVIKMAHU4xUzZY8eqhaxxGLrseVurxiLhjhS7OA==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("4aa76a4c-c59d-409a-84c1-06e6487a137a"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 21, 3, 26, 4, 676, DateTimeKind.Utc).AddTicks(5681));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 21, 3, 26, 4, 676, DateTimeKind.Utc).AddTicks(334));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                column: "DateAdded",
+                value: new DateTime(2021, 1, 21, 3, 26, 4, 676, DateTimeKind.Utc).AddTicks(5543));
+        }
+    }
+}
